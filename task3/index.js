00e33363c68ff0b713ab38629599cb2619a8a00c
Sagabym <sagabym17@gmail.com>
2022-08-22 17:23:40 +0300

    fix

--- task3/index.js
+++ task3/index.js
@@ -1,18 +1,14 @@
-function ChessBoard() {
-  const limit = 8;
-  let result = "";
-  for (let i = 0; i < limit; i++) {
-    for (let j = 0; j < limit; j++) {
-      if (i % 2 !== 0) {
-        result += j % 2 === 0 ? " " : "#";
-      } else {
-        j % 2 === 0;
-
-        result += j % 2 === 0 ? "#" : " ";
-      }
+const limit = 8;
+let result = "";
+for (let i = 0; i < limit; i++) {
+  for (let j = 0; j < limit; j++) {
+    if (i % 2 !== 0) {
+      result += j % 2 === 0 ? " " : "#";
+    } else {
+      j % 2 === 0;
+      result += j % 2 === 0 ? "#" : " ";
     }
-    result += "\n";
   }
-  return result;
+  result += "\n";
 }
-console.log(ChessBoard());
+console.log(result);
